fead62ae5eb31703e2ca95608852c1fafc2a0f3a
zxc <zxc90919zxc@gmail.com>
2020-09-08 14:08:34 +0800

    coffee

---
+++ js/scrollpage.js
@@ -0,0 +1,9 @@
+$('a[href^="#"]').on('click', function (event) {
+  let $target = $(this.getAttribute('href'))
+  if ($target.length) {
+    event.preventDefault()
+    $('html,body').stop().animate({
+      scrollTop: $target.offset().top
+    }, 2000, 'easeInOutBack')
+  }
+})
